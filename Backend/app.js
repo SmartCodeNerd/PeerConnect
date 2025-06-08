5f507775f7484f54f0ae9b0e5a2a1bcc87fe6ba5
Goldie2311 <coding.messiah007@gmail.com>
2025-06-08 23:43:54 +0530

    Basic Backend Done-2

--- Backend/app.js
+++ Backend/app.js
@@ -7,18 +7,25 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./src/config/db.js";
 import connectToServer from "./src/controllers/socketManager.js";
+import userRoutes from "./src/routes/userRoutes.js";
 
 const port = process.env.PORT || 3000;
+//To Create an instance of express
 const app = express();
+//Creates a http server from the express instance
 const server = createServer(app);
+//Connects both the express and the socket.io Server
 const io = connectToServer(server);
 
-app.use(cors());
+app.use(cors()); // Cross Origin Resource Sharing
+
+//Middlewares to convert the incoming data into readable format
 app.use(express.json({limit:"50kb"}));
 app.use(express.urlencoded({limit:"50kb",extended:true}));
 
+app.use("/auth",userRoutes);
+
 app.get("/home",(req,res) => {
-    console.log(req.body);
     console.log("Done");
     res.send("Success");
 })

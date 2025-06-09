93f72d8e5fd051793ba373ce9966d459e1908630
Goldie2311 <coding.messiah007@gmail.com>
2025-06-09 16:55:13 +0530

    Basic Auth Completed

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./src/config/db.js";
 import connectToServer from "./src/controllers/socketManager.js";
-import userRoutes from "./src/routes/userRoutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
 
 const port = process.env.PORT || 3000;
 //To Create an instance of express
@@ -23,7 +23,7 @@ app.use(cors()); // Cross Origin Resource Sharing
 app.use(express.json({limit:"50kb"}));
 app.use(express.urlencoded({limit:"50kb",extended:true}));
 
-app.use("/auth",userRoutes);
+app.use("/auth",authRoutes);
 
 app.get("/home",(req,res) => {
     console.log("Done");
---
+++ Backend/src/controllers/authController.js
@@ -0,0 +1,76 @@
+import User from "../models/userModel.js";
+import httpStatus from "http-status";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+
+const register = async (req,res) => {
+    const {name,username,password} = req.body;
+
+    if(!name || !username || !password) 
+        return res.status(httpStatus.BAD_REQUEST).json({message:"All Fields are required"});
+    try{
+        const isExisting = await User.findOne({username:username});
+        if(isExisting) {
+            //Early Return Statements
+            return res.status(httpStatus.FOUND).json({message:"User Already Exists"});
+        }
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const newUser = await User.create({
+            name,
+            username,
+            password:hashedPassword,
+        });
+
+        console.log(newUser);
+        
+        return res.status(httpStatus.CREATED).json({message:"User Registered Successfully"});
+    }
+    catch(e) {
+        return res.json({message:`Something Went Wrong-${e}`});
+    }
+};
+
+const login = async (req,res) => {
+    const {username,password} = req.body;
+    if(!username || !password) 
+        return res.status(httpStatus.BAD_REQUEST).json({message:"All Fields are required"});
+    try {
+        const user = await User.findOne({username});
+        if(!user) {
+            return res.status(httpStatus.NOT_FOUND).json({message:"User Not Found"});
+        }
+        const isMatch = bcrypt.compare(password,user.password);
+        if(!isMatch) {
+            return res.status(httpStatus.BAD_REQUEST).json({message:"Invalid Password"});
+        }
+        let token = crypto.randomBytes(20).toString("hex");
+        user.token = token;
+       await  user.save();
+        return res.status(httpStatus.OK).json({message:"User Logged in Successfully",token:token});
+    }
+    catch(e) {
+        return res.status(500).json({message:`Something Went Wrong-${e}`});
+    }
+};
+
+const logout = async (req,res) => {
+    const {username,token} = req.body;
+    const user = await User.findOne({username});
+    if(!user) {
+        return res.status(httpStatus.NOT_FOUND).json({message:"User Not Found"});
+    }
+    const isMatch = (user.token === token);
+    if(!isMatch) {
+        return res.status(httpStatus.BAD_REQUEST).json({message:"Invalid Token"});
+    }
+    await user.updateOne({ $unset: { token: 1 } });
+    console.log(user);
+    return res.status(httpStatus.OK).json({message:"Logged out Successfully"});
+}
+
+export {
+    register,
+    login,
+    logout
+};

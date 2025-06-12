a191137aa2c2a875e39af0a49a97148307e3554c
Goldie2311 <coding.messiah007@gmail.com>
2025-06-12 22:23:32 +0530

    Styling-1

--- Frontend/src/pages/landing.jsx
+++ Frontend/src/pages/landing.jsx
@@ -4,8 +4,25 @@ const Landing = () => {
   return (
     <div className="landingPageContainer"> 
       <nav>
-        
-      </nav>
+                <div className='navHeader'>
+                    <h2>PeerConnect</h2>
+                </div>
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
     </div>
   );
 };

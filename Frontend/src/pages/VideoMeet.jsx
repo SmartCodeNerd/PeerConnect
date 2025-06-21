1921153f10450dec8eb109a52c2afa0e6c7f1a15
Goldie2311 <coding.messiah007@gmail.com>
2025-06-21 21:58:24 +0530

    Added Video Meet JSX Component

--- Frontend/src/pages/VideoMeet.jsx
+++ Frontend/src/pages/VideoMeet.jsx
@@ -1,4 +1,5 @@
 import react from "react"
+import '../styles/videoComponent.css'
 
 const serverUrl = "http://localhost:3000";
 
@@ -14,10 +15,34 @@ export default function VideoMeetComponent() {
     let socketRef = useRef();
     let socketIdRef = useRef();
     let localVideoRef = useRef();
-
     let [videoAvailable,setVideoAvailable] = useState(true);
+    let [audioAvailable,setAudioAvailable] = useState(true);
+    let [video,setVideo] = useState();
+    let [audio,setAudio] = useState();
+    let [screen,setScreen] = useState();
+    let [showModal,setShowModal] = useState();
+    let [screenAvailable,setScreenAvailable] = useState();
+    let [messages,setMessages] = useState([]);
+    let [message,setMessage] = useState([]);
+    let [newMessage,setNewMessage] = useState(0);
+    let [askForUsername,setAskForUsername] = useState(true);
+    let [username,setUsername] = useState("");
+    let [videos,setVideos] = useState();
+
+    const videoRef = useRef();
+
+    //Checking that the current browser is Chromium based or not...for WebRTC's working
+    if(isChrome() === false) {}
     
     return (
-        <div>Video Meet-{window.location.href}</div>
+        // {askForUsername === true ? 
+        //     <div>
+
+
+        //     </div> 
+        //     : 
+        //     <></>
+        // }
+        <div>Hello</div>
     )
 }

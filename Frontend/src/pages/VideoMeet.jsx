17a4f119e936248949de4293366ab369321ceb70
Goldie2311 <coding.messiah007@gmail.com>
2025-06-22 01:37:15 +0530

    Video Display

--- Frontend/src/pages/VideoMeet.jsx
+++ Frontend/src/pages/VideoMeet.jsx
@@ -18,7 +18,7 @@ export default function VideoMeetComponent() {
     let socketRef = useRef();
     let socketIdRef = useRef();
     let localVideoRef = useRef();
-    let [videoAvailable,setVideoAvailable] = useState(false);
+    let [videoAvailable,setVideoAvailable] = useState(true);
     let [audioAvailable,setAudioAvailable] = useState(true);
     let [video,setVideo] = useState();
     let [audio,setAudio] = useState();
@@ -39,6 +39,7 @@ export default function VideoMeetComponent() {
     
     const getPermissions = async () => {
         try {
+            //Taking Video Permission
             const videoPermission = await navigator.mediaDevices.getUserMedia({video:true});
             if(videoPermission) {
                 setVideoAvailable(true);
@@ -46,17 +47,35 @@ export default function VideoMeetComponent() {
             else {
                 setVideoAvailable(false);
             }
-
+            //Taking Audio Permission
             const audioPermission = await navigator.mediaDevices.getUserMedia({audio:true});
             if(audioPermission) {
                 setAudioAvailable(true);
             }
             else {
                 setAudioAvailable(false);
+            }   
+
+            //This is for screen share..not taking permission but asking for which screen to share
+            if(navigator.mediaDevices.getDisplayMedia) {
+                setScreenAvailable(true);
+            }
+            else {
+                setScreenAvailable(false);
+            }
+            if(videoAvailable || audioAvailable) {
+                const userMediaStream = await navigator.mediaDevices.getUserMedia({video:videoAvailable,audio:audioAvailable});
+
+                if(userMediaStream) {
+                    window.localStream = userMediaStream;
+                    if(localVideoRef.current) {
+                        localVideoRef.current.srcObject = userMediaStream;
+                    }
+                }
             }
         }
         catch(err) {
-
+            console.log(err);
         }
     }
 
@@ -72,8 +91,8 @@ export default function VideoMeetComponent() {
                 <TextField id="outlined-basic" label="Username" value={username} onChange={e => setUsername(e.target.value)} variant="outlined" />
                 <Button variant="contained">Connect</Button>
                  
-                <div>
-                    <video ref={localVideoRef} autoplay muted></video>
+                <div className="videoDiv">
+                    <video ref={localVideoRef} autoPlay muted></video>
                 </div>
             </div> 
             : 
